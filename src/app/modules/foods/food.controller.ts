240ff536287b41d053a5c3429a673124a57e427b
meraj2704 <mh1669101@gmail.com>
2024-08-16 01:30:17 +0600

    updated code

--- src/app/modules/foods/food.controller.ts
+++ src/app/modules/foods/food.controller.ts
@@ -3,7 +3,13 @@ import Food from "./foods.models";
 import { IFoods } from "./food.interface";
 import Category from "../category/category.model";
 import { findCategory, findFood } from "./food.utils";
+import { FoodService } from "./food.service";
+import { sendResponse } from "../../utils/response";
+import { dataValidation } from "../../utils/dataValidation";
 
+//  ---------------------------------------------
+//  ------------------ Create Food --------------  done , remove redundancy
+//  ---------------------------------------------
 export const createFood = async (
   req: Request,
   res: Response,
@@ -12,36 +18,33 @@ export const createFood = async (
   console.log("called");
   const { name, quantity, price, categoryId, unit, description } = req.body;
   try {
-    if (!name) {
-      return res.status(400).json({ message: "Name is required" });
+    const requiredFields = ["name", "price", "categoryId", "quantity", "unit"];
+    const dataCheck = dataValidation(requiredFields, req.body);
+    if (dataCheck) {
+      return sendResponse(res, 400, {
+        success: false,
+        message: dataCheck,
+      });
     }
 
-    if (!price) {
-      return res.status(400).json({ message: "Price is required" });
-    }
-    if (!categoryId) {
-      return res.status(400).json({ message: "Category id is required" });
-    }
-
-    // const existFoodName = await Food.findOne({ name });
     const existFoodName = await findFood(name);
     if (existFoodName) {
-      return res
-        .status(400)
-        .json({ message: "Food already exist with this name" });
+      return sendResponse(res, 400, {
+        success: false,
+        message: "Food already exists with this name",
+      });
     }
-
-    // const existCategory = await Category.findById({ _id: categoryId });
     const existCategory = await findCategory(categoryId);
-    console.log("exist category : " + existCategory)
     if (!existCategory) {
-      return res.status(404).json({ message: "Category not found" });
+      return sendResponse(res, 400, {
+        success: false,
+        message: "Category not found",
+      });
     }
-    
-    const imagePath = req.file? req.file.path:undefined;
-    const imageFileName = req.file ? req.file.filename : undefined;
 
-    const newFood: IFoods = new Food({
+    const imagePath = req.file ? req.file.path : undefined;
+    const imageFileName = req.file ? req.file.filename : undefined;
+    const newFood = {
       name,
       quantity,
       price,
@@ -49,12 +52,14 @@ export const createFood = async (
       unit,
       description,
       imagePath,
-      imageFileName
+      imageFileName,
+    };
+    const result = await FoodService.createFood(newFood);
+    return sendResponse(res, 201, {
+      success: true,
+      message: "Food created successfully",
+      data: result,
     });
-    await newFood.save();
-    res
-      .status(201)
-      .json({ message: "Food created successfully", food: newFood });
   } catch (error) {
     console.log("error");
     console.log(error);
@@ -62,20 +67,32 @@ export const createFood = async (
   }
 };
 
+//  ---------------------------------------------
+//  ------------------ Get all Food -------------- done
+//  ---------------------------------------------
+
 export const getAllFood = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const foods = await Food.find();
-    res.status(200).json(foods);
+    const result = await FoodService.getFoods();
+    return sendResponse(res, 200, {
+      success: true,
+      message: "Foods retrieved successfully",
+      data: result,
+    });
   } catch (error) {
     console.log("error");
     console.log(error);
     next(error);
   }
 };
+
+//  ---------------------------------------------
+//  ------------------ Get Single Food -------------- done
+//  ---------------------------------------------
 
 export const getSingleFood = async (
   req: Request,
@@ -84,16 +101,27 @@ export const getSingleFood = async (
 ) => {
   const { id } = req.params;
   try {
-    const food = await Food.findById(id);
-    if (!food) {
-      return res.status(404).json({ message: "Food not found" });
+    const result = await FoodService.getFoods(id);
+    if (!result) {
+      return sendResponse(res, 404, {
+        success: false,
+        message: "Food not found",
+      });
     }
-    res.status(200).json(food);
+    return sendResponse(res, 200, {
+      success: true,
+      message: "Foods retrieved successfully",
+      data: result,
+    });
   } catch (error) {
     console.log(error);
     next(error);
   }
 };
+
+//  ---------------------------------------------
+//  ------------------ Delete Food --------------  ongoing
+//  ---------------------------------------------
 
 export const deleteFood = async (
   req: Request,
@@ -102,16 +130,28 @@ export const deleteFood = async (
 ) => {
   const { id } = req.params;
   try {
-    const food = await Food.findByIdAndDelete(id);
-    if (!food) {
-      return res.status(404).json({ message: "Food not found" });
+    // const food = await Food.findByIdAndDelete(id);
+    const result = await FoodService.deleteFood(id);
+    if (!result) {
+      return sendResponse(res, 404, {
+        success: false,
+        message: "Food not found",
+      });
     }
-    res.status(200).json({ message: "Food deleted successfully" });
+    return sendResponse(res, 200, {
+      success: true,
+      message: "Foods deleted successfully",
+      data: result,
+    });
   } catch (error) {
     console.log(error);
     next(error);
   }
 };
+
+//  ---------------------------------------------
+//  ------------------ Update Food --------------  pending
+//  ---------------------------------------------
 
 export const updateFood = async (
   req: Request,
@@ -122,29 +162,44 @@ export const updateFood = async (
   const { name, price, categoryId, description } = req.body;
   try {
     if (!name && !price && !categoryId && !description) {
-      return res
-        .status(400)
-        .json({ message: "At least one field is required" });
+      return sendResponse(res, 400, {
+        success: false,
+        message: "At least one field is required",
+      });
     }
-    const imagePath = req.file? req.file.path:undefined;
+    const imagePath = req.file ? req.file.path : undefined;
     const imageFileName = req.file ? req.file.filename : undefined;
 
-    const updateData = {name, price, categoryId, description, imagePath, imageFileName};
-    
-    const food = await Food.findByIdAndUpdate(
-      id,
-     updateData,
-      { new: true }
-    );
+    const updateData = {
+      name,
+      price,
+      categoryId,
+      description,
+      imagePath,
+      imageFileName,
+    };
+
+    const food = await Food.findByIdAndUpdate(id, updateData, { new: true });
     if (!food) {
-      return res.status(404).json({ message: "Food not found" });
+      return sendResponse(res, 404, {
+        success: false,
+        message: "Food not found",
+      });
     }
-    res.status(200).json({ message: "Food updated successfully", food: food });
+    return sendResponse(res, 200, {
+      success: true,
+      message: "Food updated successfully",
+      data: food,
+    });
   } catch (error) {
     console.log(error);
     next(error);
   }
 };
+
+//  ---------------------------------------------
+//  ------------------ GEt Food By Category --------------  pending
+//  ---------------------------------------------
 
 export const getFoodByCategory = async (
   req: Request,
@@ -155,25 +210,34 @@ export const getFoodByCategory = async (
   try {
     const category = await Category.findById(id);
     if (!category) {
-      return res.status(404).json({ message: "Category not found" });
+      return sendResponse(res, 404, {
+        success: false,
+        message: "Category not found",
+      });
     }
     const foods = await Food.find({ categoryId: id });
     if (!foods) {
-      return res.status(404).json({ message: "Foods not found" });
+    return sendResponse(res, 404, {
+        success: false,
+        message: "Foods not found",
+      });
     }
-    res.status(200).json(foods);
+    return sendResponse(res, 200, {
+      success: true,
+      message: "Foods retrieved successfully",
+      data: foods,
+    });
   } catch (error) {
     console.log(error);
     next(error);
   }
 };
 
-
 export const FoodController = {
-    createFood,
-    getAllFood,
-    getSingleFood,
-    deleteFood,
-    updateFood,
-    getFoodByCategory
-}
+  createFood,
+  getAllFood,
+  getSingleFood,
+  deleteFood,
+  updateFood,
+  getFoodByCategory,
+};
--- src/app/modules/foods/food.utils.ts
+++ src/app/modules/foods/food.utils.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 import Category from "../category/category.model";
 import { IFindFood } from "./food.interface";
 import Food from "./foods.models";
@@ -10,12 +10,13 @@ export const findFood = async (name: IFindFood) => {
 };
 
 export const findCategory = async (categoryId:any) => {
-  try {
-    console.log(categoryId);
-    const category = await Category.findById({_id:categoryId}) // Ensure to call exec() to return a promise
-    return category;
-  } catch (error) {
-    console.error('Error finding category:', error);
-    throw new Error('Internal server error');
+    console.log("category Id ", categoryId);
+
+    if (!Types.ObjectId.isValid(categoryId)) {
+      console.error("Invalid categoryId: ", categoryId);
+      return null; 
   }
+    const category = await Category.findById(categoryId)
+    console.log("category ", category);
+    return category;
 };
